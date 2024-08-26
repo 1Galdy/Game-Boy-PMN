c82c2d6a2d22149432c7abc5e1e4fff06f9dcff2
1Galdy <117950402+1Galdy@users.noreply.github.com>
2024-08-26 22:44:32 +0200

    Version numéro deux du projet

--- src/components/GameBoyAdvance.js
+++ src/components/GameBoyAdvance.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import '../styles/Advance.css';
 
-
 const dbAdvance = {
     bag: "/assets/icons/bag-icon.png",
     camion: "/assets/icons/camion-icon.png",
     carton: "/assets/icons/carton-icon.png"
-}
+};
 
 function GameBoyAdvanceSP() {
   return (
     <div className='mainContainer'>
-
         <div>
             <div className='blackPart'>
                 <section className='blackPart_livraison'>
-                    <img src={dbAdvance.camion} alet={dbAdvance.camion} />
+                    <img src={dbAdvance.camion} alt={dbAdvance.camion} />
                     <label>Livraison offerte dès 139€ avec Mondial Relay</label>
                 </section>
-                <section className='blackPart_Advance' >
+                <section className='blackPart_Advance'>
                     <h2>GAMEBOY ADVANCE</h2>
                 </section>
             </div>
@@ -34,40 +32,263 @@ function GameBoyAdvanceSP() {
                 </section>
             </div>
         </div>
-      <h1>Advance Page</h1>
-      <p>This is the Advance Page. Ou la page home</p>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
-      lorem ipsum dolor sit amet, consectetur adip<br></br>
+
+        <Carrousel />
     </div>
   );
+}
+
+const images = [
+    '/assets/GBA/SIDE/GBA-Side-GBA-Side-ClearGreen_shell_GBA_0027.jpg',
+    '/assets/GBA/FRONT/GBA-Front-GBC_LEDGBA_SHELL_CLEAR-GREEN.jpg',
+    '/assets/GBA/BACK/GBA-Back-GBA_SHELL_BACK_CLEARGREEN0027.jpg',
+];
+
+function Carrousel() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  // Fonction pour changer l'image en fonction du cercle cliqué
+  const handleCircleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="mainContainer">
+        <div class="firstContainer_advance">
+            <div className="carrousel-container">
+                <img
+                    src={images[activeIndex]}
+                    alt={`Carrousel image ${activeIndex + 1}`}
+                    className="carrousel-image"
+                />
+
+                <div className="carrousel-indicators">
+                    {images.map((_, index) => (
+                    <span
+                        key={index}
+                        className={`indicator ${index === activeIndex ? 'active' : ''}`}
+                        onClick={() => handleCircleClick(index)}
+                    ></span>
+                    ))}
+                </div>
+            </div>
+            <div className="configuration_advance">
+                    <h1>CONFIGURATION</h1>
+                    <div className='configurationAdvance_Option'>
+                        <section className='configurationAdvance_OptionList'>
+                            <details>
+                                <summary>
+                                    <h4>BASE CONSOLE</h4>
+                                    <span>Création à partir d'une console que vous fournissez</span>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                            <Coque />
+                            <details>
+                                <summary>
+                                    <h4>COQUE ARRIÈRE</h4>
+                                    <span>Uniquement si la coque arrière est différente de l'avant</span>
+                                </summary>
+                                <ul>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <li className='coqueArriere_color'></li>
+                                    <button className='button paddingButton'>sans</button>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>ÉCRAN IPS RÉTROÉCLAIRÉ</h3>
+                                </summary>
+                                <ul>
+                                    <li className='ecranColor'></li>
+                                    <li className='ecranColor'></li>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>BOUTONS</h3>
+                                </summary>
+                                <ul>
+                                <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>PADS</h3>
+                                </summary>
+                                <ul>
+                                <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>LANIÈRE</h3>
+                                </summary>
+                                <ul>
+                                <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>COQUE SPECIAL ÉDITIONS</h3>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>STICKERS*</h3>
+                                </summary>
+                                <ul>
+                                <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                    <li className='Colorfor_all'></li>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>INSTALLATION BATTERIE</h3>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>INSTALLATION LED RBG</h3>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>INSTALLATION LED TRIGGER</h3>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>INSTALLATION D-PAD TACTILE</h3>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>INSTALLATION AMP AUDIO</h3>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                            <details>
+                                <summary>
+                                    <h3>ACCESSOIRES</h3>
+                                </summary>
+                                <ul class="button-container">
+                                    <button className='button'>Je fournis la console</button>
+                                    <button className='buttonSansBorder'>Je n'ai pas de console à fournir</button>
+                                </ul>
+                            </details>
+                        </section>
+                    </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+function Coque(){
+
+    const [imageSrc, setImageSrc] = React.useState('/assets/GBA/SIDE/GBA-Side-GBA-Side-ClearGreen_shell_GBA_0027.jpg');
+
+    const changeImage = () => {
+        setImageSrc((prevSrc) =>
+        prevSrc === '/assets/GBA/SIDE/GBA-Side-GBA-Side-ClearGreen_shell_GBA_0027.jpg'
+            ? '/assets/GBA/SIDE/GBA-Side-GBA-Side-White_shell_GBA_0027.jpg'
+            : '/assets/GBA/SIDE/GBA-Side-GBA-Side-Rose_shell_GBA_0027.jpg'
+        );
+    };
+
+    const handleButtonClick = (index) => {
+        setImageSrc(images[index]);
+      };
+
+    return(
+        <>
+            <details>
+                <summary>
+                    <h4>COQUE</h4>
+                    <span>Comprend avant et arrière</span>
+                </summary>
+
+                    <ul>
+                    {images.map((src, index) => (
+                        <li className='coqueColor'
+                        key={index}
+                        onClick={() => handleButtonClick(index)}
+                        style={{ margin: '5px', padding: '10px 20px', fontSize: '16px' }}
+                        ></li>
+                    ))}     
+                    </ul>
+            </details>
+        </>
+    );
 }
 
 export default GameBoyAdvanceSP;
